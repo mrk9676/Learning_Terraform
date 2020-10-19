81cfa802b11cf61d61b625a7390d01269e6ab620
mrk9676 <mrkaptr2@gmail.com>
2020-10-19 21:57:38 +0530

    Added route tables

--- aws_ntier/variables.tf
+++ aws_ntier/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_region" {
 	type		= string
-	default		= "us-west-1"
+	default		= "us-east-1"
 }
 variable "vpc_cidr" {
 	type		= string
@@ -10,4 +10,12 @@ variable "vpc_cidr" {
 variable "vpc_subnets" {
 	type		= list(string)
 	default		= ["web", "app", "db", "mgmt", "web2", "app2", "db2"]
+}
+variable "vpc_azs" {
+	type		= list(string)
+	default		= [ "a", "b", "c", "d", "e", "f", "a" ]
+}
+variable "vpc_routes" {
+	type		= list(string)
+	default		= [ "public", "private" ]
 }
--- aws_ntier/vpc_creation.tf
+++ aws_ntier/vpc_creation.tf
@@ -1,16 +1,42 @@
 resource "aws_vpc" "my_vpc" {
-		cidr_block	= var.vpc_cidr
-		tags		= {
-			Name	= "Ntier_VPC"
+		cidr_block		= var.vpc_cidr
+		tags			= {
+			Name		= "Ntier_VPC"
 		}	
 }
 
 resource "aws_subnet" "subnets" {
-		vpc_id		= aws_vpc.my_vpc.id
-		count		= length(var.vpc_subnets)
-		cidr_block	= cidrsubnet(var.vpc_cidr,"8",count.index)
-		tags		= {
-			Name	= var.vpc_subnets[count.index]
+		vpc_id			= aws_vpc.my_vpc.id
+		count			= length(var.vpc_subnets)
+		availability_zone       = format("%s%s",var.vpc_region,var.vpc_azs[count.index])
+		cidr_block		= cidrsubnet(var.vpc_cidr,"8",count.index)
+		tags			= {
+			Name		= var.vpc_subnets[count.index]
 		}
 }
-	
+
+resource "aws_route_table" "route_table_names" {
+		vpc_id                  = aws_vpc.my_vpc.id
+		count			= length(var.vpc_routes)
+		route			  {
+			cidr_block	= "0.0.0.0/0"
+			gateway_id 	= aws_internet_gateway.my_IGW.id
+		}
+		tags			= {
+			Name		= var.vpc_routes[count.index]
+		}
+		depends_on		= [
+			aws_internet_gateway.my_IGW ]
+}
+
+
+resource "aws_internet_gateway" "my_IGW" {
+		vpc_id			= aws_vpc.my_vpc.id
+		tags			= {
+			Name		= "My_Ntier_IGW"
+		
+		
+		}
+		depends_on		= [
+			aws_subnet.subnets ]
+}
